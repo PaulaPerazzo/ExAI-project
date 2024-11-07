73ca1d1dff9bb65a75efc33c5f730c0fe7d5f714
paulaperazzo <mpps@cin.ufpe.br>
2024-11-07 09:38:50 -0300

    HAR model training

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=HAR-opt
+#SBATCH --job-name=HAR-trial-1-train
 #SBATCH --ntasks=1
 #SBATCH --mem 16G
 #SBATCH --gres=gpu:2
 #SBATCH -c 8
 #SBATCH -o job.log
-#SBATCH --output=job_output_har_opt_2024-11-06.txt
-#SBATCH --error=job_error_har_opt_2024-11-06.txt
+#SBATCH --output=job_output_har_train_2024-11-07.txt
+#SBATCH --error=job_error_har_train_2024-11-07.txt
 
 # carregar versão python
 module load Python/3.9
@@ -15,4 +15,4 @@ module load Python/3.9
 source ./env/bin/activate
 
 # executar .py
-python3 src/optimization.py
+python3 src/train.py
