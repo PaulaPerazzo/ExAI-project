b5d8a349e528d28842e53dcb39a447d60d6b8bb3
paulaperazzo <mpps@cin.ufpe.br>
2024-11-12 09:21:49 -0300

    har train trial 2

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=HAR-trial-2-opt
+#SBATCH --job-name=HAR-trial-2-train
 #SBATCH --ntasks=1
 #SBATCH --mem 16G
 #SBATCH --gres=gpu:2
 #SBATCH -c 8
 #SBATCH -o job.log
-#SBATCH --output=job_output_har_opt_trial_2_2024-11-11.txt
-#SBATCH --error=job_error_har_opt_trial_2_2024-11-11.txt
+#SBATCH --output=job_output_har_train_trial_2_2024-11-12.txt
+#SBATCH --error=job_error_har_train_trial_2_2024-11-12.txt
 
 # carregar versão python
 module load Python/3.9
@@ -15,4 +15,4 @@ module load Python/3.9
 source ./env/bin/activate
 
 # executar .py
-python3 src/optimization.py
+python3 src/train.py
